210db9d65b33d9fb749ca34d6b4e189ac6b010cc
Heather King <hkingdevelopment@gmail.com>
2022-01-07 15:49:05 +0000

    adds Flight class and tests

---
+++ src/main/java/flight/Flight.java
@@ -0,0 +1,105 @@
+package flight;
+
+import passenger.Passenger;
+import plane.Plane;
+import plane.PlaneType;
+import staff.CabinCrewMember;
+import staff.Pilot;
+
+import java.util.ArrayList;
+
+public class Flight {
+
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+    private Plane plane;
+    private ArrayList<Pilot> pilots;
+    private ArrayList<CabinCrewMember> cabinCrew;
+    private ArrayList<Passenger> passengers;
+
+    public Flight(String flightNumber, String destination, String departureAirport, String departureTime) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+        this.plane = null;
+        this.pilots = new ArrayList<>();
+        this.cabinCrew = new ArrayList<>();
+        this.passengers = new ArrayList<>();
+    }
+
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void addPilot(Pilot pilot) {
+        this.pilots.add(pilot);
+    }
+
+    public int pilotCount() {
+        return this.pilots.size();
+    }
+
+    public PlaneType getPlaneType() {
+        return this.plane.getPlaneType();
+    }
+
+    public void addPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public int cabinCrewCount() {
+        return this.cabinCrew.size();
+    }
+
+    public void addCabinCrew(CabinCrewMember cabinCrewMember) {
+        this.cabinCrew.add(cabinCrewMember);
+    }
+
+    public int passengerCount() {
+        return this.passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public int getPlaneCapacity() {
+        return this.plane.getCapacityFromEnum();
+    }
+
+    public int getTotalAvailableSeats() {
+        return this.getPlaneCapacity() - this.passengers.size();
+    }
+}
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,128 @@
+import flight.Flight;
+import org.junit.Before;
+import org.junit.Test;
+import passenger.Passenger;
+import plane.Plane;
+import plane.PlaneType;
+import staff.CabinCrewMember;
+import staff.Pilot;
+import staff.Rank;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+    Pilot pilot;
+    CabinCrewMember cabinCrewMember1;
+    CabinCrewMember cabinCrewMember2;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Passenger passenger4;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING747);
+        pilot = new Pilot("Sully", Rank.CAPTAIN, "SS123");
+        cabinCrewMember1 = new CabinCrewMember("Jo Jones", Rank.FLIGHT_ATTENDANT);
+        cabinCrewMember2 = new CabinCrewMember("Joe Smith", Rank.FLIGHT_ATTENDANT);
+        passenger1 = new Passenger("Tom Jones", 1);
+        passenger2 = new Passenger("Betty Jones", 1);
+        passenger3 = new Passenger("Johnny Jones", 1);
+        passenger4 = new Passenger("Bill Jones", 1);
+        flight = new Flight("BA1234", "London", "Edinburgh", "17:00");
+    }
+
+
+    @Test
+    public void hasFlightNumber() {
+        assertEquals("BA1234", flight.getFlightNumber());
+    }
+
+    @Test
+    public void canSetFlightNumber() {
+        flight.setFlightNumber("BA9999");
+        assertEquals("BA9999", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination() {
+        assertEquals("London", flight.getDestination());
+    }
+
+    @Test
+    public void canSetDestination() {
+        flight.setDestination("Bristol");
+        assertEquals("Bristol", flight.getDestination());
+    }
+
+    @Test
+    public void hasDepartureAirport() {
+        assertEquals("Edinburgh", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void canSetFDepartureAirport() {
+        flight.setDepartureAirport("Glasgow");
+        assertEquals("Glasgow", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void hasDepartureTime() {
+        assertEquals("17:00", flight.getDepartureTime());
+    }
+
+    @Test
+    public void canSetDepartureTime() {
+        flight.setDepartureTime("12:00");
+        assertEquals("12:00", flight.getDepartureTime());
+    }
+
+    @Test
+    public void canAddPilot() {
+        flight.addPilot(pilot);
+        assertEquals(1, flight.pilotCount());
+    }
+
+    @Test
+    public void canAddPlane() {
+        flight.addPlane(plane);
+        assertEquals(PlaneType.BOEING747, flight.getPlaneType());
+    }
+
+    @Test
+    public void canAddCabinCrew() {
+        flight.addCabinCrew(cabinCrewMember1);
+        flight.addCabinCrew(cabinCrewMember2);
+        assertEquals(2, flight.cabinCrewCount());
+    }
+
+    @Test
+    public void canAddPassengers() {
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        assertEquals(2, flight.passengerCount());
+    }
+
+    @Test
+    public void canGetPlaneCapacity() {
+        flight.addPlane(plane);
+        assertEquals(2, flight.getPlaneCapacity());
+    }
+
+    @Test
+    public void canGetTotalAvailableSeats() {
+        flight.addPlane(plane);
+        flight.addPassenger(passenger1);
+        assertEquals(1, flight.getTotalAvailableSeats());
+    }
+
+
+
+
+
+
+
+}
